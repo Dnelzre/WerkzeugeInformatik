bdaa3cb7d14566ab91c6fbe13173190e033e1fc2
darmiel <you@example.com>
2024-11-18 17:14:36 +0100

    merge-conflit anderung

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car1 = new Car("red", "BMW", 5);
+        Car car1 = new Car("blue", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
         factory.buildCar(car1);
         factory.buildCar(car2);
+        System.out.println("merge-conflict änderung");
     }
 }
 
